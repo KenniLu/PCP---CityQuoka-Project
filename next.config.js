0b6df23787b1f55cb65aacd522241cca84e8c6f4
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2024-11-29 21:41:54 +1100

    Removing cache and retrying

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,11 +20,6 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
-  webpack: (config) => {
-    config.resolve.alias['@'] = join(__dirname, 'src')
-    config.resolve.alias['src'] = join(__dirname, 'src')
-    return config
-  }
 }
 
 export default withPayload(nextConfig)
