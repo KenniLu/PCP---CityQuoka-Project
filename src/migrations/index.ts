bc52aa4dc9e426110295f98d88fb196a43227420
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2024-12-05 12:43:46 +1100

    Add media prefix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241129_010502_initial from './20241129_010502_initial';
+import * as migration_20241205_014304_add_media_prefix from './20241205_014304_add_media_prefix';
 
 export const migrations = [
   {
     up: migration_20241129_010502_initial.up,
     down: migration_20241129_010502_initial.down,
-    name: '20241129_010502_initial'
+    name: '20241129_010502_initial',
+  },
+  {
+    up: migration_20241205_014304_add_media_prefix.up,
+    down: migration_20241205_014304_add_media_prefix.down,
+    name: '20241205_014304_add_media_prefix'
   },
 ];
