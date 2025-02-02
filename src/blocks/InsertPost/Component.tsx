c20dc1673615ecdf97660504dfe8559cd1204f04
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2025-02-03 02:09:33 +1100

    Commenting out Insert Post options

--- src/blocks/InsertPost/Component.tsx
+++ src/blocks/InsertPost/Component.tsx
@@ -1,12 +1,12 @@
-import type { Post, InsertPost as InsertPostProps } from '@/payload-types'
+// import type { Post, InsertPost as InsertPostProps } from '@/payload-types'
 
-import { cn } from 'src/utilities/cn'
-import React from 'react'
+// import { cn } from 'src/utilities/cn'
+// import React from 'react'
 
-type Props = {
-  className?: string
-} & InsertPostProps
+// type Props = {
+//   className?: string
+// } & InsertPostProps
 
-export const InsertPost: React.FC<Props> = ({ className, post }) => {
-  return <div className={cn('mx-auto my-8 w-full', className)}>Post - {(post as Post).id}</div>
-}
+// export const InsertPost: React.FC<Props> = ({ className, post }) => {
+//   return <div className={cn('mx-auto my-8 w-full', className)}>Post - {(post as Post).id}</div>
+// }
