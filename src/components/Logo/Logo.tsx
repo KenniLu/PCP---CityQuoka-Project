d85e4dd96f609f8190e030fab46140e8f66d0eac
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2025-02-06 19:11:41 +1100

    Add redirect to home on Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Logo = () => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <div className="flex justify-center w-full py-6 sm:py-8">
-      <Image
-        className="object-contain self-center max-w-full aspect-[3.37]"
-        src="/logo.svg"
-        alt="CityQuokka"
-        width={300}
-        height={0}
-      />
+      <Link href="/">
+        <Image
+          className="object-contain self-center max-w-full aspect-[3.37]"
+          src="/logo.svg"
+          alt="CityQuokka"
+          width={300}
+          height={0}
+        />
+      </Link>
     </div>
   )
 }
