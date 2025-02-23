04bfcc92e41a9e0a3accaa2c5a1893c85ac04814
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2025-02-23 20:57:29 +1100

    Enable healtcheck URL

---
+++ src/app/(frontend)/api/healthcheck/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  return NextResponse.json('success', { status: 200 })
+}
+
+export async function POST() {
+  return NextResponse.json({ error: 'Method not allowed' }, { status: 405 })
+}
+
+export async function PUT() {
+  return NextResponse.json({ error: 'Method not allowed' }, { status: 405 })
+}
+
+export async function DELETE() {
+  return NextResponse.json({ error: 'Method not allowed' }, { status: 405 })
+}
