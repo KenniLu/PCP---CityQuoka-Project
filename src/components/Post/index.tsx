99674202105f6b6a64e8a8f7ec447b207f81aff2
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2025-04-14 16:34:15 +1000

    Only show breadcrumbs on posts when there is more than one category path ( 86cyjd9pq )

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -23,7 +23,7 @@ type Args = {
 }
 
 const PostBreadCrumb = (breadcrumbs: Category['breadcrumbs']) => {
-  return (
+  return (breadcrumbs || []).length > 1 ? (
     <Breadcrumb>
       <BreadcrumbList>
         {(breadcrumbs || []).map((crumb, indx) => (
@@ -40,7 +40,7 @@ const PostBreadCrumb = (breadcrumbs: Category['breadcrumbs']) => {
         ))}
       </BreadcrumbList>
     </Breadcrumb>
-  )
+  ) : null
 }
 
 export default async function Post({ post }: Args) {
