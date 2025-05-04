b9a71d15cc2d10efe19471854a088cd106405bc6
Dasaradhy Srinivasan <dasaradhy@gmail.com>
2025-05-04 12:21:03 +1000

    Introduce additional logging

--- src/app/(payload)/api/[...slug]/route.ts
+++ src/app/(payload)/api/[...slug]/route.ts
@@ -10,11 +10,12 @@ import {
   REST_POST,
   REST_PUT,
 } from '@payloadcms/next/routes'
+import { withPayloadRequestContext } from '@/utilities/requestContextWrapper'
 
 export const GET = REST_GET(config)
 export const POST = REST_POST(config)
 export const DELETE = REST_DELETE(config)
-export const PATCH = REST_PATCH(config)
+export const PATCH = withPayloadRequestContext(REST_PATCH(config))
 
 export const PUT = REST_PUT(config)
 export const OPTIONS = REST_OPTIONS(config)
