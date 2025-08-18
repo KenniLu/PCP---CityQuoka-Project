2562b8bdee3f5a4a2999b04bef40f1a945ca0b70
Dasaradhy Srinivasan <dasaradhy.srinivasan@cityquokka.com>
2025-08-18 23:53:15 +1000

    User initialization scripts completion

--- src/scripts/init-role.ts
+++ src/scripts/init-role.ts
@@ -4,28 +4,53 @@ import config from '@payload-config'
 async function run() {
   const payload = await getPayload({ config })
 
-  const userRoles = await payload.find({
+  const rolesCount = await payload.count({
     collection: 'user-roles',
-    limit: 1,
-    where: {
-      name: {
-        equals: 'SuperAdminn',
-      },
-    },
+    where: {}
   })
 
-  if (userRoles.docs.length === 0) {
-    const adminRole = await payload.create({
-      collection: 'user-roles',
-      data: {
-        name: 'SuperAdminn',
-        permissions: {
-          admin: true,
-        },
-      },
-    })
-  }
+  const usersCount = await payload.count({
+    collection: 'users',
+    where: {}
+  })
 
+  if (rolesCount.totalDocs === 0 && usersCount.totalDocs === 0) {
+
+    if(process.env.DEFAULT_USER && process.env.DEFAULT_USERNAME && process.env.DEFAULT_PASSWORD){
+
+      const transactionID = await payload.db.beginTransaction()
+      try {
+        const adminRole = await payload.create({
+          collection: 'user-roles',
+          data: {
+            name: 'SuperAdmin',
+            permissions: {
+              admin: true,
+            }
+          },
+          req: { transactionID: transactionID! },
+        })
+        const user = await payload.create({
+          collection: 'users',
+          data: {
+            email: process.env.DEFAULT_USER,
+            name: process.env.DEFAULT_USERNAME,
+            password: process.env.DEFAULT_PASSWORD,
+            userRoles: [adminRole],
+          },
+          req: { transactionID: transactionID! },
+        })
+        await payload.db.commitTransaction(transactionID!)
+      } catch (error) {
+        await payload.db.rollbackTransaction(transactionID!)
+        console.log(`Failed to Initialize. Error: ${error}`)
+      }
+    }else{
+      console.log(`Skipping Initialization. Please add DEFAULT_USER, DEFAULT_USERNAME and DEFAULT_PASSWORD in your .env file`)
+    }
+  }else{
+    console.log(`Database has existing users and roles. Skipping Initialization`)
+  }
   process.exit(0)
 }
 
