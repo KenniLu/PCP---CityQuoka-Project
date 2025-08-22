08d56e35183f54a96b10db0f7a4aac5589c0c9e3
KenLu <luchihao113@gmail.com>
2025-08-22 12:02:55 +1000

    Initial commit

---
+++ src/app/(frontend)/(footer)/user-profile/page.tsx
@@ -0,0 +1,19 @@
+export default function Userprofile() {
+    return (
+      <>
+       <h1 className="text-3xl font-bold mb-4 pr-5">User Profile</h1>
+       <p>
+          User Name
+        </p>
+        <br></br>
+        <p>
+          User Email
+        </p>
+        <br></br>
+        <p>
+          User Address
+        </p>
+      </>
+    );
+  };
+  
